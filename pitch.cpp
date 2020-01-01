45f7648a8b7599fe626cc3e09faf263a63f822ac
adammccartney <35410331+adammccartney@users.noreply.github.com>
2020-01-01 15:46:56 +0100

    Update pitch.cpp

--- pitch.cpp
+++ pitch.cpp
@@ -1,70 +1,34 @@
+
 #include "pitch.h"
+#include <stdexcept>   // for exception, runtime_error, out_of_range
 
-namespace PitchPath {
+int main()
+try {
+	using namespace PitchPath;
 
-	Pitch operator++(Pitch& p)
-	{
-		p = (p == Pitch::h) ? Pitch::c : Pitch(int(p) + 1); // wrap around
-		return p;
-	}
-	
-	//-------------------------------------------------------------
-	// construct PitchClass
+	int fis = 7;
+	std::vector<int> fft= { 7,7,4 };
+	PitchClass fisOne = { fis,fft };
 
-	// default
-	PitchClass::PitchClass() : p{ 0 }, tp{ {'\0'} } { }
-	PitchClass::PitchClass(int p, std::vector<int> tp) 
-		: p{ p }, tp{ tp }
-	{
-		//if (!is_valid())throw Invalid{}; // if valid, initializes pitch with a member of enum class Fis Moll
-	}
+	int e = 6;
+	std::vector<int> ttff = { 4,4,7,7 };
+	PitchClass eOne = { e,ttff };
 
-	void PitchClass::PrintRecursionMap()
-	{
-		for (auto& e : recursion_map) {
-			std::cout << '{' << e.first << "," << e.second << '}' << '\n';
-		}
-	}
+	fisOne.Transpose(fis, fft, 100000);
+	fisOne.PrintRecursionMap();
 
+	eOne.Transpose(e, ttff, 100000);
+	eOne.PrintRecursionMap();
 
-	//----------------------------------------------------------------
-
-	/*
-	bool PitchClass::is_valid()
-	{
-		if(p < 0 || 11 > p)return false; // there are 12 pitches in an octave
-		{
-			// if pitch is valid, initialize
-			PitchClass::p = p;
-			PitchClass::tp = tp;
-		}
-		return true;
-	}
-	*/
-	//------------------------------------------------------------------------
-
-	void PitchClass::AddPitch(int n)
-	{
-		bool lastPitch = false;
-		// if day exceeds last pitch in the octave, roll on to next octave and set pitch to 0
-		for (int i = 0; i < n; ++i)
-		{
-			if (p == 11)
-				lastPitch = true;
-				p = (p == 11) ? 0 : ++p; // at h, roll on to c if pitch = 11, otherwise ++
-		}
-	}
-
-	//------------------------------------------------------------------------
-
-	// arguments are starting pitch, tranpositions, number of interations
-	void PitchClass::Transpose(const int& p, const std::vector<int>& tp, int x)  
-	{
-		for (int i = 0; i < x; ++i) {
-			for (int j = 0; j < tp.size(); ++j) {
-				AddPitch(tp[j]);
-				CountPitchRecursion(GetPitch());
-			}
-		}
-	}
-};//PitchPath
+}
+catch (std::runtime_error& e) {
+	std::cout << "Caught a runtime_error exception: "
+		<< e.what() << '\n';
+}
+catch (std::exception& e) {
+	std::cout << "Caught an exception of an unexpected type: "
+		<< e.what() << '\n';
+}
+catch (...) {
+	std::cout << "Caught an unknown exception\n";
+}
