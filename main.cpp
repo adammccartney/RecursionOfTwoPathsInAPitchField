204842332579a141fa250d16048eecf8f52f28dd
adammccartney <35410331+adammccartney@users.noreply.github.com>
2020-01-01 13:23:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,50 @@ try {
 
 	std::cout << "here is the current pitch: " << TestPitch.ConvertPitchToString(TestPitch) << '\n';
 	
-	TestPitch.SetPitch(TestPitch.TransposeDownSixth(TestPitch));
+	TestPitch.SetPitch(TestPitch.TransposeDownSixth(TestPitch)); // test transpose function
 	std::cout << "here it is down a sixth: " 
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';    // confirm it works
+	SetStepCountFisMemPath(TestPitch.GetPitchString());          // count action in map
+	TestPitch.SetPitch(TestPitch.TransposeDownSixth(TestPitch)); // repeat
+	std::cout << "and again: "
 		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	std::cout << "and again: "
+		<< TestPitch.ConvertPitchToString(TestPitch) << '\n';
+	SetStepCountFisMemPath(TestPitch.GetPitchString());
+	for (auto& e : FisMemPath) {
+		std::cout << '{' << e.first << "," << e.second << '}' << '\n';
+	}
 
 }
 catch (std::runtime_error& e) {
